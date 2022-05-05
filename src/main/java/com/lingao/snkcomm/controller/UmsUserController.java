a43429d4f561cae33b10c4ecd2df9d4f416159f6
lingao <lingao@intelink.net>
2022-05-05 15:14:58 +0800

    增加header并实现退出登录

--- src/main/java/com/lingao/snkcomm/controller/UmsUserController.java
+++ src/main/java/com/lingao/snkcomm/controller/UmsUserController.java
@@ -51,4 +51,8 @@ public class UmsUserController extends BaseController{
         UmsUser user = umsUserService.getUserByUsername(userName);
         return ApiResult.success(user);
     }
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ApiResult<Object> logOut() {
+        return ApiResult.success(null, "注销成功");
+    }
 }
