fe989c6040cf370e8d99a76a9864031eac6d770e
lingao <lingao@intelink.net>
2022-05-07 23:01:59 +0800

    实现修改、删除帖子、打赏作者功能

--- src/main/java/com/lingao/snkcomm/controller/BmsPostController.java
+++ src/main/java/com/lingao/snkcomm/controller/BmsPostController.java
@@ -9,9 +9,14 @@ import com.lingao.snkcomm.model.entity.UmsUser;
 import com.lingao.snkcomm.model.vo.PostVO;
 import com.lingao.snkcomm.service.IBmsPostService;
 import com.lingao.snkcomm.service.IUmsUserService;
+import com.vdurmont.emoji.EmojiParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -36,6 +41,7 @@ public class BmsPostController extends BaseController{
         Page<PostVO> list = bmsPostService.getList(new Page<>(pageNo, pageSize), tab);
         return ApiResult.success(list);
     }
+
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ApiResult<BmsPost> create(@RequestHeader(value = USER_NAME) String userName, @RequestBody CreateTopicDTO dto) {
         UmsUser user = umsUserService.getUserByUsername(userName);
@@ -47,14 +53,42 @@ public class BmsPostController extends BaseController{
         }
         return ApiResult.success(topic);
     }
+
     @RequestMapping(value = "/view", method = RequestMethod.GET)
     public ApiResult<Map<String, Object>> view(@RequestParam("id") String id) {
         Map<String, Object> map = bmsPostService.viewTopic(id);
         return ApiResult.success(map);
     }
+
     @RequestMapping(value = "/recommend", method = RequestMethod.GET)
     public ApiResult<List<BmsPost>> getRecommend(@RequestParam("topicId") String id) {
         List<BmsPost> topics = bmsPostService.getRecommend(id);
         return ApiResult.success(topics);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ApiResult<BmsPost> update(@RequestHeader(value = USER_NAME) String userName, @Valid @RequestBody BmsPost post) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        if(!umsUser.getId().equals(post.getUserId())){
+            return ApiResult.failed("非本人无权修改");
+        }
+        post.setModifyTime(new Date());
+        post.setContent(EmojiParser.parseToAliases(post.getContent()));
+        bmsPostService.updateById(post);
+        return ApiResult.success(post);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ApiResult<String> delete(@RequestHeader(value = USER_NAME) String userName, @PathVariable("id") String id) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        BmsPost byId = bmsPostService.getById(id);
+        if(ObjectUtils.isEmpty(byId)){
+            return ApiResult.failed("来晚一步，话题已不存在");
+        }
+        if(!byId.getUserId().equals(umsUser.getId())){
+            return ApiResult.failed("非本人无权删除");
+        }
+        bmsPostService.removeById(id);
+        return ApiResult.success(null,"删除成功");
     }
 }
